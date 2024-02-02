bded60049544d59b9357ba0a45a17e4aa338cea1
Arthur Mikael <arthurmik860@gmail.com>
2024-02-02 08:57:31 -0300

    first steps

---
+++ src/main/java/resources/UserResource.java
@@ -0,0 +1,15 @@
+package resources;
+
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
+
+@RestController
+@RequestMapping(value = "/users")
+public class UserResource {
+
+
+
+}
