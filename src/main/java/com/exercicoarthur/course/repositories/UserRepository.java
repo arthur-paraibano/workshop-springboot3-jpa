15082cdc436668520083978d639b119787467f35
Arthur-Paraibano <arthurmik860@gmail.com>
2024-02-02 14:46:28 -0300

    JPA repository, dependency injection, database seeding

---
+++ src/main/java/com/exercicoarthur/course/repositories/UserRepository.java
@@ -0,0 +1,10 @@
+package com.exercicoarthur.course.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.exercicoarthur.course.entites.User;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+
+    
+}
